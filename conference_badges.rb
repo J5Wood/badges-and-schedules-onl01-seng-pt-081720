602ea8f0524fb60cf702aafd95e0a2086c8a6ed2
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 19:20:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,9 +24,8 @@ def assign_rooms(attendees)
 end
 
 
-def printer
-  batch_badge_creator()
-  badges.each{|badge| puts badge}
-  binding.pry
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  
 
 end
